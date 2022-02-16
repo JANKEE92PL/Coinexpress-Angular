037094fc1c6f97884d4fb17c453051396f2c4dec
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-16 13:34:14 +0100

    add HTTPClient to service

--- src/app/coin.service.ts
+++ src/app/coin.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoinService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 }
